16f27c47a1c8d8758c8861e0f2e96e246b2532cb
Azarias Galama <129897944+azariasabera@users.noreply.github.com>
2024-07-22 01:40:31 +0300

    Done task 5

--- src/components/MyContainer.js
+++ src/components/MyContainer.js
@@ -3,16 +3,26 @@ import {useState} from 'react';
 
 const MyContainer = () => {
    const [items, setItems] = useState([
-        { id: 1, text: 'Item 1' },
-        { id: 2, text: 'Item 2' },
+        { id: 1, text: 'Item 1', clicked: false },
+        { id: 2, text: 'Item 2', clicked: false },
    ]);
 
     const onClick = () => {
         const textarea = document.getElementById('textarea');
         const text = textarea.value;
-        const newItems = [...items, { id: items.length + 1, text }];
+        const newItems = [...items, { id: items.length + 1, text, clicked: false }];
         setItems(newItems);
         textarea.value = '';
+    }
+
+    const updateItem = (id) => {
+        const newItems = items.map(item => {
+            if (item.id === id) {
+                return { ...item, clicked: !item.clicked };
+            }
+            return item;
+        });
+        setItems(newItems);
     }
         
 
@@ -26,6 +36,7 @@ const MyContainer = () => {
         <MyList 
             header="Really epic list component"
             items={items}
+            updateItem={updateItem}
         />
     </div>
   )
--- src/components/MyList.js
+++ src/components/MyList.js
@@ -1,14 +1,29 @@
-function MyList({ header, items }) {
-  return (
-    <div>
-        <h1>{header}</h1>
-        <ol>
-            {items.map(item => (
-                <li key={item.id}>{item.text}</li>
-            ))}
-        </ol>
-    </div>
-  )
+import './MyList.css';
+
+function MyList({ header, items, updateItem }) {
+
+    const getClassName = (clicked) => {
+        if (clicked) {
+            return 'clicked';
+        } else {
+            return '';
+        }
+    }
+    return (
+        <div>
+            <h1>{header}</h1>
+            <ol>
+                {items.map(item => (
+                    <li key={item.id} onClick={()=>updateItem(item.id)} className={getClassName(item.clicked)}
+                    >{item.text}</li>
+                ))}
+            </ol>
+        </div>
+    )
 }
+
+// Alternative way to give className (very easy way)
+// className={ item.clicked ? 'clicked' : '' }
+
 
 export default MyList

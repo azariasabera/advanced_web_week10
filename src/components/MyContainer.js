4e9f0ebaaf26973c092a5726b1a008523aafc2c7
Azarias Galama <129897944+azariasabera@users.noreply.github.com>
2024-07-22 01:14:36 +0300

    Update MyContainer.js

--- src/components/MyContainer.js
+++ src/components/MyContainer.js
@@ -1,15 +1,31 @@
 import MyList from './MyList';
+import {useState} from 'react';
 
 const MyContainer = () => {
+   const [items, setItems] = useState([
+        { id: 1, text: 'Item 1' },
+        { id: 2, text: 'Item 2' },
+   ]);
+
+    const onClick = () => {
+        const textarea = document.getElementById('textarea');
+        const text = textarea.value;
+        const newItems = [...items, { id: items.length + 1, text }];
+        setItems(newItems);
+        textarea.value = '';
+    }
+        
+
   return (
     <div>
         <h1>My Container</h1>
+        <textarea id='textarea' placeholder='Add item'></textarea>
+        <button onClick={onClick}>Add</button>
+
+        {/* Passing props to MyList component*/}
         <MyList 
             header="Really epic list component"
-            items={[
-                {id: 1, text: "Item 1"},
-                {id: 2, text: "Item 2"},
-            ]}
+            items={items}
         />
     </div>
   )
